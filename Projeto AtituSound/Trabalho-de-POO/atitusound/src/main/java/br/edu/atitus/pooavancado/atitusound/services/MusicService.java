4547e7a3d7aa0c5630d0d383ac8c25b7126b8342
Marco Antonio Flores da Silva <71043797+maarcoafs@users.noreply.github.com>
2023-11-13 00:23:29 -0300

    V1.2

--- Projeto AtituSound/Trabalho-de-POO/atitusound/src/main/java/br/edu/atitus/pooavancado/atitusound/services/MusicService.java
+++
@@ -1,8 +0,0 @@
-package br.edu.atitus.pooavancado.atitusound.services;
-
-import br.edu.atitus.pooavancado.atitusound.entities.MusicEntity;
-
-public interface MusicService extends GenericService<MusicEntity> {
-
-		
-}
--- Projeto AtituSound/Trabalho-de-POO/atitusound/src/main/java/br/edu/atitus/pooavancado/atitusound/services/PlaylistService.java
+++
@@ -1,8 +0,0 @@
-package br.edu.atitus.pooavancado.atitusound.services;
-
-import br.edu.atitus.pooavancado.atitusound.entities.PlaylistEntity;
-
-public interface PlaylistService extends GenericService<PlaylistEntity> {
-
-		
-}
